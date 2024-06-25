42bd750daba2a0084e52acbc7a9c262c5719b142
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-25 14:44:30 -0700

    feat: user login with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 end
