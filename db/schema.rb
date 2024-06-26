6aff384ca34601f1ecaffa2b8ab5a57c6d9910e8
Lance <lanceirvingmanzanal@icloud.com>
2024-06-26 15:34:26 -0600

    chore: generate an item db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_25_210037) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_213027) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.date "expiration_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
