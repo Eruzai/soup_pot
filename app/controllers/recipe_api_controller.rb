050e072d70f6787b5c7fa4dfab691d80818e081b
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-26 14:49:34 -0700

    feat: query accepts form data

--- app/controllers/recipe_api_controller.rb
+++ app/controllers/recipe_api_controller.rb
@@ -10,13 +10,19 @@ class RecipeApiController < ApplicationController
   def search
     app_id = ENV.fetch('EDAMAM_APPLICATION_ID')
     key = ENV.fetch('EDAMAM_APPLICATION_KEY')
-    response = RestClient.get("https://api.edamam.com/api/recipes/v2?type=public&q=tomato%20&app_id=#{app_id}&app_key=#{key}&dishType=Soup")
+    response = RestClient.get("https://api.edamam.com/api/recipes/v2?type=public&q=#{ingredients_query}%20&app_id=#{app_id}&app_key=#{key}&dishType=Soup")
     @json = JSON.parse response
     
     render :show
   end
 
   private
+
+  def ingredients_query
+    item = search_params
+    query = "#{item[:ingredient_1]} #{item[:ingredient_2]} #{item[:ingredient_3]} #{item[:ingredient_4]} #{item[:ingredient_5]} #{item[:ingredient_6]} #{item[:ingredient_7]} #{item[:ingredient_8]} #{item[:ingredient_9]} #{item[:ingredient_10]},"
+    query.strip
+  end
 
   def search_params
     params.permit(
