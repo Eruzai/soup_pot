6521889f1568d1cae368e1e0c5e9422247af7932
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-26 14:08:06 -0700

    feat: use env variables

--- app/controllers/recipe_api_controller.rb
+++ app/controllers/recipe_api_controller.rb
@@ -8,24 +8,28 @@ class RecipeApiController < ApplicationController
   end
 
   def search
-    search_recipes
-  end
-
-  private
-
-  def search_recipes
-    response = RestClient.get('https://api.edamam.com/api/recipes/v2?type=public&q=potato%2C%20carrot%2C%20celery%2C%20leek&app_id=bc70c8f4&app_key=f005f5f0c7a7709124e657b4457c6926&dishType=Soup')
+    app_id = ENV.fetch('EDAMAM_APPLICATION_ID')
+    key = ENV.fetch('EDAMAM_APPLICATION_KEY')
+    response = RestClient.get("https://api.edamam.com/api/recipes/v2?type=public&q=tomato%20&app_id=#{app_id}&app_key=#{key}&dishType=Soup")
     @json = JSON.parse response
     
     render :show
   end
 
+  private
+
   def search_params
-    # params.require(:review).permit(
-    #   :product_id,
-    #   :user_id,
-    #   :description,
-    #   :rating
-    # )
+    params.permit(
+      :ingredient_1,
+      :ingredient_2,
+      :ingredient_3,
+      :ingredient_4,
+      :ingredient_5,
+      :ingredient_6,
+      :ingredient_7,
+      :ingredient_8,
+      :ingredient_9,
+      :ingredient_10,
+    )
   end
 end
