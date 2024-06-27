9617d6b20fd1ce8767bc6293d1da8e182a7b45f4
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-27 16:40:57 -0700

    feat: query user recipe data for recipes to show on page

--- app/controllers/saved_recipes_controller.rb
+++ app/controllers/saved_recipes_controller.rb
@@ -1,6 +1,16 @@
 class SavedRecipesController < ApplicationController
 
-  def show
+  def my_recipes
+    @user = User.find (current_user.id)
+    @recipes = @user.recipes.all
+    render '/saved_recipes/show'
   end
-  
+
+  # def friends_recipes
+  #   @user = User.find (current_user.id)
+  #   @friends = @user.friends.all
+  #   @recipes = @friends.each do |friend| friend.recipes.all
+  #   render '/saved_recipes/show'
+  # end
+
 end
