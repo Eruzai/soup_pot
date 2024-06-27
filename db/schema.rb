dc8f37f6a186593ce566006194883a07fb9a4f29
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-27 13:40:14 -0700

    chore: update schema and Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_27_181957) do
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.integer "quantity"
-    t.date "expiration_date"
-  end
-  
   create_table "ingredients", force: :cascade do |t|
     t.integer "recipe_id"
     t.string "item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.date "expiration_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
