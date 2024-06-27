cd6a11d0e439265be53189330ea949928f7a1cd7
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-27 12:57:36 -0700

    feat: belongs to user and has many ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,12 @@
 class Recipe < ApplicationRecord
+
+  belongs_to :user
+
+  has_many :ingredients
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :source, presence: true
+  validates :recipe_url, presence: true
+
 end
