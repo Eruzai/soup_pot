5d192f81ae1fb8f4db2025a4a56569bdcaa4f69b
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-28 12:02:00 -0700

    feat: implement creation logic for creating recipes and saving them to the db

--- app/controllers/saved_recipes_controller.rb
+++ app/controllers/saved_recipes_controller.rb
@@ -4,6 +4,7 @@ class SavedRecipesController < ApplicationController
   end
 
   def my_recipes
+    @path = 'my_recipes'
     @user = User.find (current_user.id)
     @recipes = @user.recipes.all
     render '/saved_recipes/index'
@@ -16,7 +17,22 @@ class SavedRecipesController < ApplicationController
   #   render '/saved_recipes/index'
   # end
 
+  def new
+  end
+
   def create
+    @user = User.find (current_user.id)
+    @recipe = @user.recipes.new(recipe_params)
+
+    if @recipe.save
+      recipe_ingredients.each do |item|
+        @ingredient = @recipe.ingredients.new(item: item)
+        @ingredient.save
+      end
+      redirect_to '/my_recipes', notice: 'Recipe information and ingredients saved!'
+    else
+      raise 'Recipe failed to save!'
+    end
   end
 
   def edit
@@ -27,6 +43,22 @@ class SavedRecipesController < ApplicationController
     @recipe.destroy
 
     redirect_to '/my_recipes'
+  end
+
+  private
+
+  def recipe_ingredients
+    params[:ingredients].split(',')
+  end
+
+  def recipe_params
+    params.permit(
+      :name,
+      :image,
+      :description,
+      :cooking_time,
+      :directions
+    )
   end
 
 end
