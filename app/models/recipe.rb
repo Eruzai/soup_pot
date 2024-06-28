f15b29b1ef8e883ca3d48be955ec67213fbbfc12
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-28 10:47:21 -0700

    fix: destroy ingredients before recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,19 @@
 class Recipe < ApplicationRecord
 
+  before_destroy :destroy_ingredients
+  
   belongs_to :user
-
   has_many :ingredients
 
   validates :name, presence: true
   validates :description, presence: true
   validates :source, presence: true
   validates :recipe_url, presence: true
+
+  private
+
+  def destroy_ingredients
+    self.ingredients.destroy_all
+  end
 
 end
