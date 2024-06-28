2598f4deb696c9eb847cb649bd59c650c9f9fdec
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-28 10:46:40 -0700

    feat: create destroy logic

--- app/controllers/saved_recipes_controller.rb
+++ app/controllers/saved_recipes_controller.rb
@@ -1,16 +1,32 @@
 class SavedRecipesController < ApplicationController
 
+  def show
+  end
+
   def my_recipes
     @user = User.find (current_user.id)
     @recipes = @user.recipes.all
-    render '/saved_recipes/show'
+    render '/saved_recipes/index'
   end
 
   # def friends_recipes
   #   @user = User.find (current_user.id)
   #   @friends = @user.friends.all
   #   @recipes = @friends.each do |friend| friend.recipes.all
-  #   render '/saved_recipes/show'
+  #   render '/saved_recipes/index'
   # end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+    @recipe = Recipe.find params[:id]
+    @recipe.destroy
+
+    redirect_to '/my_recipes'
+  end
 
 end
