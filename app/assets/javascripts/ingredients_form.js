2458388e02907049fb7c993b95d9a89c54d2df31
Lance <lanceirvingmanzanal@icloud.com>
2024-06-28 17:13:19 -0600

    chore: delete the file

--- app/assets/javascripts/ingredients_form.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const dropdown = document.getElementById('ingredient-dropdown');
-  const textField = document.getElementById('ingredient-text-field');
-
-    dropdown.addEventListener('change', function() {
-      textField.value = dropdown.value;
-    });
-
-});
