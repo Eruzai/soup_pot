eecde923f9f49ac092be360548f27da14466e39d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-07-02 11:33:23 -0700

    feat: write logic to accept requests and remove friends and requests

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,4 +7,24 @@ class FriendsController < ApplicationController
     @recieved_requests = @user.inverse_friends.where(pending: true).map(&:user)
   end
 
+  def send_request
+  end
+
+  def accept_request
+    @request = Friend.where(user_id: params[:id], friend_id: current_user.id)
+    @request.update(pending: false)
+    redirect_to '/find_friends'
+  end
+
+  def destroy
+    @friend = Friend.find_by user_id: params[:id], friend_id: current_user.id
+    if @friend
+      @friend.destroy
+    else
+      @friend = Friend.find_by user_id: current_user.id, friend_id: params[:id]
+      @friend.destroy
+    end
+    redirect_to '/find_friends'
+  end
+
 end
