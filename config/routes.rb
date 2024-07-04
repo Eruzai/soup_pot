c633bb7f1090fd0256443744a6874e11fa7ee989
Lance <lanceirvingmanzanal@icloud.com>
2024-07-03 18:16:50 -0600

    chore: refactor the routes for the saved_recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,17 +16,19 @@ Rails.application.routes.draw do
   post '/recipes' => 'recipe_api#search'
   post '/recipes/:id' => 'recipe_api#create'
 
-  get '/my_recipes' => 'saved_recipes#my_recipes'
-  get '/my_recipes/create' => 'saved_recipes#new'
-  post '/my_recipes/create' => 'saved_recipes#create'
-  get '/my_recipes/:id' => 'saved_recipes#show'
-  patch '/my_recipes/:id' => 'saved_recipes#edit'
-  delete '/my_recipes/:id' => 'saved_recipes#destroy'
-  get '/friends_recipes' => 'saved_recipes#friends_recipes'
+
+  resources :my_recipes, controller: 'saved_recipes', only: [:show, :index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      get :friends_recipes, as: :friends_recipes
+    end
+  end
 
   get '/find_friends' => 'friends#show'
   post '/find_friends/:id' => 'friends#send_request'
   delete '/find_friends/:id' => 'friends#destroy'
   patch '/find_friends/:id' => 'friends#accept_request'
-  
+
+  resources :recipes do
+    resources :reviews, only: [:create, :destroy, :edit, :update]
+  end
 end
