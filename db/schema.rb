69e077ea9c1020968583d3c65604d10c24529a5c
Lance <lanceirvingmanzanal@icloud.com>
2024-07-03 18:17:45 -0600

    feat: create a new table for the reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_02_025818) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_235802) do
   create_table "friends", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "friend_id", null: false
@@ -53,6 +53,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_025818) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.integer "recipe_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -68,4 +79,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_025818) do
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "items", "users"
   add_foreign_key "recipes", "users"
+  add_foreign_key "reviews", "recipes"
+  add_foreign_key "reviews", "users"
 end
