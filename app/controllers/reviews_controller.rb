552eecb6d01aa2c2284ceae2c5611464f75edda1
Lance <lanceirvingmanzanal@icloud.com>
2024-07-03 18:09:40 -0600

    feat: generate a controller for reviews and add actions to create, edit, and delete a review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,40 @@
+class ReviewsController < ApplicationController
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @review = @recipe.reviews.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to "/my_recipes/#{@recipe.id}", notice: 'Review was successfully created.'
+    else
+      render 'saved_recipes/show'
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find params[:recipe_id]
+    @review = @recipe.reviews.find params[:review_id]
+  end
+
+  def update
+    @recipe = Recipe.find params[:recipe_id]
+    @review = Review.find params[:id]
+    if @review.update(review_params)
+      redirect_to  "/my_recipes/#{@recipe.id}"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find params[:recipe_id]
+    @review = @recipe.reviews.find params[:review_id]
+    @review.destroy
+    redirect_to "/my_recipes/#{@recipe.id}", notice: 'Review deleted!'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+end
