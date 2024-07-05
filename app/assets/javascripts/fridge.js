9ad6e91975943535fbb9545c978efe895d70f0b0
Kevin Levasseur <kevin_levasseur@telus.net>
2024-07-05 13:29:43 -0700

    feat: style recipe search page

--- app/assets/javascripts/fridge.js
+++ app/assets/javascripts/fridge.js
@@ -1,10 +1,12 @@
 document.addEventListener('turbolinks:load', () => {
   const toggleButton = document.getElementById('toggle-items');
   const itemsList = document.getElementById('items-list');
+  const fridgeMessage = document.getElementById('fridge-message');
 
   if (toggleButton && itemsList) {
     toggleButton.addEventListener('click', () => {
       itemsList.classList.toggle('hidden');
+      fridgeMessage.classList.toggle('hidden');
       toggleButton.textContent = itemsList.classList.contains('hidden') ? 'Open Fridge' : 'Close Fridge';
     });
   }
