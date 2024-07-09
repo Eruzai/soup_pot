02d3af1dbccd24e6a901a0659c7ab7f59d8113d9
Kevin Levasseur <kevin_levasseur@telus.net>
2024-07-09 11:31:30 -0700

    feat: pass and show appropriate event data

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+
+  def show
+    @events = Event.where(id: event_ids)
+  end
+
+  private
+
+  def event_ids
+    JSON.parse(params[:ids])
+  end
+
 end
