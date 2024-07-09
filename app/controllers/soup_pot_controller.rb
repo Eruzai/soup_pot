a5ac90fe83f86f043c2227a6eb0db75849c9bcfd
Kevin Levasseur <kevin_levasseur@telus.net>
2024-07-08 18:11:18 -0700

    feat: show a dot for mine or friends events on their day

--- app/controllers/soup_pot_controller.rb
+++ app/controllers/soup_pot_controller.rb
@@ -2,7 +2,14 @@ class SoupPotController < ApplicationController
 
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
-    @events = Event.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @my_events = Event.where(user_id: current_user.id, start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @friends_events = Event.where(user_id: friends, start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 
+  private
+
+  def friends
+    user = User.find(current_user.id)
+    user.friends.where(pending: false).pluck(:friend_id) + user.inverse_friends.where(pending: false).pluck(:user_id)
+  end
 end
