f152e8b881671e6801fef2ce230dff0bd6f0eae2
Lance <lanceirvingmanzanal@icloud.com>
2024-07-09 16:33:06 -0600

    feat: implement a create event action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,26 @@ class EventsController < ApplicationController
     @events = Event.where(id: event_ids)
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.events.new(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
+
+  def event_params
+    params.require(:event).permit(:name, :start_time)
+  end
 
   def event_ids
     JSON.parse(params[:ids])
