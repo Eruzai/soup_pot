d9549b49df14cd44267e7eccee6f512f8ba3545e
Kevin Levasseur <kevin_levasseur@telus.net>
2024-07-11 12:07:15 -0700

    fix: create search action instead of having it in show

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,19 @@
 class FriendsController < ApplicationController
 
   def show
+    @user = User.find(current_user.id)
+    @friends = @user.friends.where(pending: false).map(&:friend) + @user.inverse_friends.where(pending: false).map(&:user)
+    @sent_requests = @user.friends.where(pending: true).map(&:friend)
+    @recieved_requests = @user.inverse_friends.where(pending: true).map(&:user)
+  end
+
+  def search
     @search_results = User.where(search_query).limit(15)
     @user = User.find(current_user.id)
     @friends = @user.friends.where(pending: false).map(&:friend) + @user.inverse_friends.where(pending: false).map(&:user)
     @sent_requests = @user.friends.where(pending: true).map(&:friend)
     @recieved_requests = @user.inverse_friends.where(pending: true).map(&:user)
+    render :show
   end
 
   def send_request
