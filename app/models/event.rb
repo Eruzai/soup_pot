a6927b6ebd4062a1a69184dd323e38aab58c0f80
Lance <lanceirvingmanzanal@icloud.com>
2024-07-10 22:27:22 -0600

    feat: validate the form is not empty on submission

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   belongs_to :recipe, optional: true
 
   has_many :attendees, dependent: :destroy
+
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :description, presence: true
 end
